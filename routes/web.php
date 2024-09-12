4d31f1a6ba3bcff7c40d46606bba9c27b79e6c5d
dziub <dziuba925@gmail.com>
2024-09-12 22:45:23 +0200

    laravel ui

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,35 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use App\Http\Controllers\CalculatorController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 
 // Trasy do wyświetlenia tekstu i obliczeń
 Route::get('/test', [CalculatorController::class, 'test']);  // Wyświetla tekst
 Route::get('/add', [CalculatorController::class, 'add']);  // Oblicza sumę
+
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
